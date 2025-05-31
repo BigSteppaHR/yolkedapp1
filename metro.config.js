43b74d02b167e8f72fcce60ef1ffabb963d60631
BigSteppaHR <157386930+BigSteppaHR@users.noreply.github.com>
2025-05-31 16:54:40 -0400

    Fix Metro configuration

--- metro.config.js
+++ metro.config.js
@@ -2,7 +2,4 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-// Disable unstable_enablePackageExports to fix Supabase/ws issues
-config.resolver.unstable_enablePackageExports = false;
-
-module.exports = config; 
+module.exports = config;
